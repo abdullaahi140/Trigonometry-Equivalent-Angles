82bd02ed7ba006021dd890792a403d3dc4b877b7
Abdullaahi Farah <abdullaahi140@gmail.com>
2017-11-19 14:12:30 +0000

    Filename changes

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 from equivtrigangles_mpmath import TrigEquivAngle as tea
 import mpmath
 
-x = tea.equiv_cot(2,range1=-1,range2=360,degrees=True)
+x = tea.equiv_cosec(2,range1=-1,range2=360,degrees=True)
 print (x)
